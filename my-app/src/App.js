e1db9047d9d5f8d657fda430563d7bf1a06f15cd
HoangThang21 <nt0912794191@gmail.com>
2023-06-20 10:20:27 +0700

    c

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Main from "./components/Main";
 
 function App() {
   return (
-    <div>Check name</div>
+    <div>
+      <Main></Main>
+    </div>
   );
 }
 
