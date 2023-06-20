3be312f0385dac181e9f70726013e4ddce0a7d46
HoangThang21 <nt0912794191@gmail.com>
2023-06-20 10:39:19 +0700

    main css

--- my-app/src/components/Main.js
+++ my-app/src/components/Main.js
@@ -2,20 +2,68 @@ import React from "react";
 import Navbar from "./navbar/Navbar";
 import Body from "./body/Body";
 import Sidebar from "./sidebar/Sidebar";
+import styled from "styled-components";
 
+const MainStyled = styled.div`
+  *,
+  *:before,
+  *:after {
+    box-sizing: border-box;
+  }
+  * {
+    margin: 0;
+    padding: 0;
+    font: inherit;
+  }
+  img,
+  picture,
+  svg,
+  video {
+    display: block;
+    max-width: 100%;
+  }
+  input,
+  select,
+  textarea {
+    background-color: transparent;
+    outline: none;
+  }
+  button {
+    cursor: pointer;
+
+    outline: none;
+    border: 0;
+  }
+  body {
+    min-height: 100vh;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 1;
+  }
+  display: flex;
+  .Navbar {
+    width: 18%;
+  }
+  .Body {
+    width: 60%;
+  }
+  .Sidebar {
+    width: 22%;
+  }
+`;
 const Main = () => {
   return (
-    <div>
-      <div>
+    <MainStyled>
+      <div className="Navbar">
         <Navbar></Navbar>
       </div>
-      <div className="">
+      <div className="Body">
         <Body></Body>
       </div>
-      <div className="">
+      <div className="Sidebar">
         <Sidebar></Sidebar>
       </div>
-    </div>
+    </MainStyled>
   );
 };
 
