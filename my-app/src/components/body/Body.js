ca7e920a9802711b6af7bf2eb52dc0e9fcb6c052
thinhnhatle11 <thinhnhatle11@gmail.com>
2023-06-20 15:17:13 +0700

    check

--- my-app/src/components/body/Body.js
+++ my-app/src/components/body/Body.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
+
 const Body = () => {
     return (
         <div>
-            a
-        </div>
+            <video src='../../../public/film122-1.mp4'></video>   
+            </div>
     );
 };
 
