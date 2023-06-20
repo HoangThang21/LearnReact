15d9b9f451b56a54923b16c4c98aa99980bda18b
HoangThang21 <nt0912794191@gmail.com>
2023-06-20 10:27:37 +0700

    clo

--- my-app/src/components/Main.js
+++ my-app/src/components/Main.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Navbar from "./navbar/Navbar";
+import Body from "./body/Body";
+import Sidebar from "./sidebar/Sidebar";
 
 const Main = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <Navbar></Navbar>
+      </div>
+      <div className="">
+        <Body></Body>
+      </div>
+      <div className="">
+        <Sidebar></Sidebar>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
